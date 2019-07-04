fea39e8eff31fca75a7b143a1b1b50c41c64addf
BLBaylis <bradleybaylis@hotmail.com>
2019-07-04 23:48:29 +0100

    add responsive hero video

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-require('dotenv').config();
+require("dotenv").config();
 require("normalize.css/normalize.css");
 require("./styles/index.scss");
 
 import "regenerator-runtime/runtime.js";
-import PopperJs from "popper.js";
 import $ from "jquery";
 import "bootstrap/js/dist/collapse";
 import "bootstrap/js/dist/modal";
 import "bootstrap/js/dist/alert";
 
 $(() => {
-    $.easing.easeInOutExpo = function (x, t, b, c, d) {
-        if (t==0) return b;
-        if (t==d) return b+c;
-        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
-    };
     const contactForm = $(".contact-form");
-    $(".nav-link").on("click", event => {animateScroll(event); collapseMobileNavbar(event)});
+    positionHomeVideo("DOMContentLoaded");
+    $.easing.easeInOutExpo = function(x, t, b, c, d) {
+        if (t == 0) return b;
+        if (t == d) return b + c;
+        if ((t /= d / 2) < 1) return (c / 2) * Math.pow(2, 10 * (t - 1)) + b;
+        return (c / 2) * (-Math.pow(2, -10 * --t) + 2) + b;
+    };
+    $(".nav-link").on("click", event => {
+        animateScroll(event);
+        collapseMobileNavbar(event);
+    });
     $(".scroll-link").on("click", animateScroll);
     contactForm.on("submit", handleFormSubmit);
     contactForm.on("animationend", () => {
@@ -28,19 +31,52 @@ $(() => {
     });
 });
 
+const positionHomeVideo = eventType => {
+    const video = $(".home-video");
+    const homeVideoWrapperInner = $(".home-video-wrapper-inner");
+    const sideBit = $(".side-bit");
+    const eventHandler = () => {
+        const aspectRatio = $(window).width() / $(window).height();
+        const height = document.querySelector(".home-video").offsetHeight;
+        const width = 1.77 * height;
+        const right = -0.39 * width;
+        if (aspectRatio < 1.0875) {
+            homeVideoWrapperInner.css("right", right);
+        } else if (aspectRatio <= 1.775) {
+            homeVideoWrapperInner.css("right", "initial");
+        }
+    };
+    if (eventType === "resize") {
+        eventHandler();
+    }
+    if (eventType === "DOMContentLoaded") {
+        window.addEventListener("load", eventHandler);
+        window.addEventListener("resize", eventHandler);
+    }
+};
+
 const animateScroll = event => {
     const eventTarget = event.target;
-    console.log(eventTarget);
-    if (location.pathname.replace(/^\//, '') == eventTarget.pathname.replace(/^\//, '') && location.hostname == eventTarget.hostname) {
+    if (
+        location.pathname.replace(/^\//, "") ==
+            eventTarget.pathname.replace(/^\//, "") &&
+        location.hostname == eventTarget.hostname
+    ) {
         let linkTarget = $(eventTarget.hash);
-        linkTarget = linkTarget.length ? linkTarget : $('[name=' + linkTarget.slice(1) + ']');
+        linkTarget = linkTarget.length
+            ? linkTarget
+            : $("[name=" + linkTarget.slice(1) + "]");
         if (linkTarget.length) {
-            $('html, body').animate({
-                scrollTop: (linkTarget.offset().top)
-            }, 1000, "easeInOutExpo");
+            $("html, body").animate(
+                {
+                    scrollTop: linkTarget.offset().top
+                },
+                1000,
+                "easeInOutExpo"
+            );
         }
     }
-}
+};
 
 const collapseMobileNavbar = event => {
     const navbarCollapse = $(".navbar-collapse");
@@ -66,7 +102,7 @@ const handleFormSubmit = async event => {
         errCaught = true;
     }
     if (!errCaught) {
-      outcome = await res.json();
+        outcome = await res.json();
     }
     toggleLoaderAnimation();
     const wasEmailSuccessful = !errCaught && res.ok && outcome === "success";
